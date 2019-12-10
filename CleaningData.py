d3114704ebc9abaea59c1131bce21343f5c7236f
Facundo-B <fmblanco97@gmail.com>
2019-12-10 17:59:42 -0300

    Update CleaningData.py

--- CleaningData.py
+++ CleaningData.py
@@ -26,4 +26,16 @@ pd.pivot_table(dataframe, index = [columns_not_to_pivot], columns = [columns_to_
 
 #python has a built-in .split(delim), which splits a strings into parts separated by "delim" (returns a list)
 
+#-----------------------------------------------------------------------------------------------------------------------------------------
 
+#Concatenating Data
+
+pd.concat([dataframe1, dataframe2], ignore_index = True) #axis = 1 for column-wise concatenation.
+
+import glob #for finding files based on patterns
+
+glob.glob("*.csv")
+
+#Merging Data (SQL join)
+
+pd.merge(left = df1, right = df2, on = column, left_on = column_left, right_on =column_right) #left/right only if key column is not the same in both df's
