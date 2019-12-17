3fe8b5305c43eb530030df15763bb732c1eb173c
Facundo-B <fmblanco97@gmail.com>
2019-12-17 17:27:47 -0300

    Update pandas_manipulation.py

--- pandas_manipulation.py
+++ pandas_manipulation.py
@@ -14,5 +14,27 @@ df.iloc[row_nbr, col_nbr]
 
 #Slicing
 
-#A column is actually a series, a onne-dimensional array with a labeled index
+#A column is actually a series, a one-dimensional array with a labeled index
+
+dataframe.loc[] #labels (or lists of labels)
+
+dataframe.iloc[] #index  (or list of indexes)
+
+#Filtering
+
+boolean_array = dataframe['column'] > int
+
+dataframe.loc[boolean_array]
+
+dataframe.dropna(how='any'/'all') #remove rows where any/all of these two columns contains missing data.
+
+dataframe.dropna(thresh=1000, axis='columns') #drops columns with more thatn 1000 missing values
+
+#Transforming Dataframes
+
+dataframe.apply() #apply an arbitrary Python function to every element.
+
+dataframe['column'].map(dictionary) #transform values according to a Python dictionary look-up
+
+#apply and map are not efficient
 
